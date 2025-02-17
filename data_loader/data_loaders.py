0a5481e1e9ea1cc3ae8973806876bc840c6af7a2
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2025-02-17 17:48:19 +0800

    updata

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,42 @@
-from torchvision import datasets, transforms
+import os
+import numpy as np
+import torch
 from base import BaseDataLoader
+from torch.utils.data import Dataset
 
+class GWDataset(Dataset):
+    def __init__(self, data_dir):
+        # get the data list
+        self.root = data_dir
+        self.clean_list = os.listdir(data_dir + 'clean/')
 
-class MnistDataLoader(BaseDataLoader):
+    def __len__(self):
+        return len(self.clean_list)
+    
+    def __getitem__(self, index):
+        clean_path = self.root + 'clean/' + self.clean_list[index]
+        noisy_path = clean_path.replace('clean', 'noisy')
+        
+        clean = np.load(clean_path)
+        noisy = np.load(noisy_path)
+
+        # normalize the data to [-1, 1]
+        clean[clean>0] = clean[clean>0] / np.max(noisy)
+        clean[clean<0] = clean[clean<0] / np.abs(np.min(noisy))
+        noisy[noisy>0] = noisy[noisy>0] / np.max(noisy)
+        noisy[noisy<0] = noisy[noisy<0] / np.abs(np.min(noisy))
+
+        # to tensor
+        clean = torch.from_numpy(clean).float()
+        noisy = torch.from_numpy(noisy).float()
+
+        return noisy, clean
+
+class GWDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.2, num_workers=8, training=True):
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = GWDataset(self.data_dir)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- model/metric.py
+++ model/metric.py
@@ -1,20 +1,6 @@
 import torch
 
-
-def accuracy(output, target):
+def mse_score(output, target):
+    # divide len
     with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
-
-
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
+        return torch.nn.functional.mse_loss(output, target).item()
